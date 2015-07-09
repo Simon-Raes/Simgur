39591f17ee953a77e879b0f387aae714249032c1
Simon Raes <simon.raes@inthepocket.mobi>
2015-07-09 21:41:56 +0200

    Initial Commit

---
+++ Simgur/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simgur
+//
+//  Created by Simon Raes on 09/07/15.
+//  Copyright (c) 2015 Simon Raes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
